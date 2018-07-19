a0bdf1899e4872c28c9002a9d36a1bc1fd97d035
dtk1024 <dtk1024@hotmail.com>
2018-07-19 21:36:02 +0200

    flappyBirdClone

---
+++ android/src/com/kori2/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.kori2.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.kori2.game.flappy;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new flappy(), config);
+	}
+}
